93d709010f8c799a94df26b2726cb70df273a42f
Andres Gismondi <andres.gismondi@despegar.com>
2018-12-12 16:45:18 -0300

    Comiezo de login con angular7

---
+++ angularLogin/angular-login/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginFormComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ angularLogin/angular-login/src/app/login-form/login-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent implements OnInit {
+
+  constructor(
+    private fb:FormBuilder) { }
+
+  ngOnInit() {
+  }
+
+}
