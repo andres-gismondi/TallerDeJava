698aaf0f6e8b8f7f32dc727159c7b0e73fdb72e5
Andres Gismondi <andres.gismondi@despegar.com>
2018-12-22 10:57:04 -0300

    Borra todo nuevamente

--- grupe23/src/main/java/app/model/dao/CommentaryDAO.java
+++
@@ -1,9 +0,0 @@
-package app.model.dao;
-
-import app.model.Commentary;
-
-public interface CommentaryDAO extends GenericDAO<Commentary> {
-
-    public Commentary getCommentary(Commentary commentary);
-
-}
--- grupe23/src/main/java/app/model/dao/CommunicationDAO.java
+++
@@ -1,9 +0,0 @@
-package app.model.dao;
-
-import app.model.Communication;
-
-public interface CommunicationDAO extends GenericDAO<Communication>{
-
-    public Communication getCommunication(Communication communication);
-
-}
